e7eb66471b4de854dfa000f94d41736d63ebe79e
Spicysnowe <85620483+Spicysnowe@users.noreply.github.com>
2023-06-07 14:24:52 +0530

    first commit

---
+++ app/src/main/java/com/techtastic/scanme/QrScanner.java
@@ -0,0 +1,17 @@
+package com.techtastic.scanme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class QrScanner extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_scanner);
+    }
+}
---
+++ app/src/main/java/com/techtastic/scanme/Splashactivity.java
@@ -0,0 +1,14 @@
+package com.techtastic.scanme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Splashactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashactivity);
+    }
+}
